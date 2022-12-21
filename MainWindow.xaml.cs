c7434c20c1b0b2b2c4b86986c35805276e76a8cc
Shamil <shigabiev.shamil@mail.ru>
2022-12-21 18:17:38 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gibdd
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LicensesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.LicensesWindow licensesWindow = new Windows.LicensesWindow();
+            licensesWindow.Show();
+            this.Close();
+        }
+
+        private void DriversBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PenaltiesBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Windows/LicensesWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using db = Gibdd.Models;
+
+namespace Gibdd.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для LicensesWindow.xaml
+    /// </summary>
+    public partial class LicensesWindow : Window
+    {
+        db.GibddEntities _context = new db.GibddEntities();
+        db.License _selectedlicense = new db.License();
+        public LicensesWindow()
+        {
+            InitializeComponent();
+            LicensesList.ItemsSource = _context.License.OrderBy(x => x.Id).ToList();
+            LicensesList.SelectionChanged += LicensesList_SelectionChanged;
+        }
+
+        private void LicensesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            _selectedlicense = LicensesList.SelectedItem as db.License;
+            if(_selectedlicense != null)
+            {
+                AboutBtn.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void RegistrateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            using (db.GibddEntities ent = new db.GibddEntities())
+            {
+                List<db.Driver> t = ent.Driver.Select(x => x).Where(x => x.License.Count==0).ToList();
+                if (t.Count == 0)
+                    MessageBox.Show("У всех водителей в системе есть водительские удостоверения");
+                else
+                {
+
+                }
+            }
+        }
+
+        private void AboutBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ConfiscatedBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
